543625934f24c3653d7acc6d6f6995b2f049ef7d
macaluzate <mcanavera@unal.edu.co>
2025-08-13 12:00:07 -0500

    Actualizando home.nix con rutas nvim

--- home.nix
+++ home.nix
@@ -1,10 +1,21 @@
 { config, pkgs, ... }:
 let 
+  # Configs externas
   zshCfg = import ./config/zsh/zsh.nix;
   nvimCfg = import ./config/nvim/nvim.nix;
   kittyCfg = import ./config/kitty/kitty.nix;
+
+  # Paquete personalizado de TeX
   myTex = pkgs.texlive.combine {
-    inherit (pkgs.texlive) scheme-small latexmk xetex luatex biblatex biber beamer amsmath;
+    inherit (pkgs.texlive)
+      scheme-small
+      latexmk
+      xetex
+      luatex
+      biblatex
+      biber
+      beamer
+      amsmath;
   };
 in
 {
@@ -18,7 +29,6 @@ in
     pkgs.python3Packages.pygments
     pkgs.texstudio
   ];
-  
   
   # Traer módulos externos
   programs.zsh = zshCfg;
@@ -34,4 +44,23 @@ in
   };
   
   programs.home-manager.enable = true;
+
+  # Config adicional para Neovim
+  xdg.configFile = {
+    # main init.lua
+    "nvim/init.lua".source = ./config/nvim/init.lua;
+
+    # core
+    "nvim/lua/core/options.lua".source =
+      ./config/nvim/lua/core/options.lua;
+    "nvim/lua/core/keymaps.lua".source =
+      ./config/nvim/lua/core/keymaps.lua;
+
+    # plugins
+    "nvim/lua/plugins/colortheme.lua".source =
+      ./config/nvim/lua/plugins/colortheme.lua;
+    "nvim/lua/plugins/neotree.lua".source =
+      ./config/nvim/lua/plugins/neotree.lua;
+  };
 }
+
